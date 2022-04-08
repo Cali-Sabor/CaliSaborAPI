bedf2cf66be80db62d5c3f75688ea84942ccab00
Luis Angel Vargas Mosquera <luisaangelvargas91@gmail.com>
2022-04-08 03:43:23 -0500

    Add: Change config to run correctly in windows using docker

--- calisabor/runserver.sh
+++ calisabor/runserver.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 python manage.py makemigrations
 
 python manage.py migrate
 
 python manage.py initadmin
 
-python manage.py runserver 0.0.0.0:8080
+python manage.py runserver 0.0.0.0:8000
